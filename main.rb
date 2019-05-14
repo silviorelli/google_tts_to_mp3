75c13be647d1a2c4dec930601c5f38372e9790a0
Silvio Relli <silvio@relli.it>
2019-05-14 14:00:18 +0200

    Tuning to audio output. Default input to SSML format.

--- main.rb
+++ main.rb
@@ -11,24 +11,28 @@ text = File.read('text.txt')
 client = Google::Cloud::TextToSpeech.new
 
 # Set the text input to be synthesized
-synthesis_input = { text: text }
+# synthesis_input = { text: text }
+synthesis_input = { ssml: text }
 
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
 voice = {
   language_code: "it-IT",
-  name: "it-IT-Wavenet-A",
-  # ssml_gender: "FEMALE"
+  name: "it-IT-Wavenet-A"
 }
 
 # Select the type of audio file you want returned
-audio_config = { audio_encoding: "MP3" }
+audio_config = { 
+  audio_encoding: "MP3",
+  pitch: 0,
+  speaking_rate: 0.9
+}
 
 # Perform the text-to-speech request on the text input with the selected
 # voice parameters and audio file type
 response = client.synthesize_speech synthesis_input, voice, audio_config
 
-filename = "output-#{Time.now.to_i}.mp3"
+filename = "output/output-#{Time.now.to_i}.mp3"
 
 # The response's audio_content is binary.
 File.open filename, "wb" do |file|
